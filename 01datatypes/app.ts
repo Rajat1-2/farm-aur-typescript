063c1f9c0a02ff6d6d8560c2d48bad0e295189d5
Rajat1-2 <rajatparashar456@gmail.com>
2026-06-30 12:01:06 +0530

    add setup and vars for typescript

---
+++ 01datatypes/app.ts
@@ -0,0 +1,6 @@
+// primitive and references-> []{}() jime ye use ho gye
+// then how to copy refernce type
+// var s=[2,3,4,5,6]
+// var k=s // k is pointing to s , not k is copy of s
+let arr=[1,3,"hifi",true,{name:"harrr"},{naem:23}]
+let arrq: number[]= [] //ye bs no ka array ka h othervise error dega
---
+++ setup/app.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+// valid js code is valid ts code 
+// even code is wrong it stilll got compiled in js 
+// code faulty ho hme kuch nhi pta so have to careful
+// tsc filename 
+// ts.config file me we pass setting for our ts code like if a var use nhi hua to mt chlao code ko etc etc
+// use tsc --init  or npx tsc --init
+// tsc --watch auto comiple ts code to js like npm run dev
+// auto save on kro 
+//# sourceMappingURL=app.js.map
+
+// why needed ts ---> 
